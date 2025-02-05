5b2ca3965a75f4d9accdbe1504d0b75e7d90b076
Heesun Lee <heesun.task@gmail.com>
2025-02-05 01:27:40 -0800

    Install libraries:bootstrap, tabler icon, react-router, styled-components and Add basic components and repository settings

--- totem-client-front/README.md
+++ totem-client-front/README.md
@@ -9,17 +9,15 @@
 - **Vite**: Development tooling for blazing-fast builds.
 - **React**: Library for building user interfaces.
 - **TypeScript**: Type-safe JavaScript superset.
--
-- **Styled-Components**: CSS-in-JS styling solution.
-- **react-router-dom**: Declarative routing for React.
-- **Bootstrap**: React components library (https://mantine.dev/)
+- **react-router**: Declarative routing/navigation for React. (https://reactrouter.com/start/library/routing)
+- **Styled-Components**: CSS-in-JS styling solution. (https://styled-components.com/)
+- **Bootstrap**: React components library (https://react-bootstrap.netlify.app/docs/getting-started/introduction/)
 
 ### **Getting Started**
 
 #### **1. Prerequisites**
 
 Ensure you have the following installed on your system:
-
 - [Node.js](https://nodejs.org/) (version 16 or higher recommended)
 - [npm](https://www.npmjs.com/)
 
--- totem-client-front/src/App.tsx
+++ totem-client-front/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+import HomePage from './pages/home/HomePage.tsx';
+import NotFoundPage from './pages/notFound/NotFoundPage.tsx';
+import ReadPage from './pages/read/ReadPage.tsx';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+const App: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/read" element={<ReadPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
