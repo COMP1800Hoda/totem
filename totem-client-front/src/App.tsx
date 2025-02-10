e537330ba2db0294da434a0200d087db2f57051b
Victor Yang <vyang11@my.bcit.ca>
2025-02-10 12:08:48 -0800

    Added paths to Router

--- totem-client-front/src/App.tsx
+++ totem-client-front/src/App.tsx
@@ -5,7 +5,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from './pages/home/HomePage.tsx';
 import NotFoundPage from './pages/notFound/NotFoundPage.tsx';
 import ReadPage from './pages/read/ReadPage.tsx';
-
+import Catalogue from './pages/catalogue/CataloguePage.tsx';
+import ProfilePage from './pages/profile/ProfilePage.tsx';
 
 const App: React.FC = () => (
   <Router>
@@ -13,7 +14,10 @@ const App: React.FC = () => (
       <Route path="/" element={<HomePage />} />
       <Route path="/read" element={<ReadPage />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route path="/c" element={<Catalogue />} />
+      <Route path="/p" element={<ProfilePage />} />
     </Routes>
+
   </Router>
 );
 
