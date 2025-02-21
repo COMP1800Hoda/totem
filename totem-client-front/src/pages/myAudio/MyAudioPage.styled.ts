541579d589853b7d084c98d3764672543dbe0b87
Victor Yang <vyang11@my.bcit.ca>
2025-02-21 11:27:30 -0800

    Created audio page and myaudio

---
+++ totem-client-front/src/pages/myAudio/MyAudioPage.styled.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const MyAudioContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Ensure the container takes the full height of the viewport */
+  padding: 0;
+  margin: 0;
+  background-color: #f8f8f8; /* Light background color for the page */
+
+  /* If you want the content to stretch and fill available space */
+  main {
+    flex: 1;
+  }
+`;
+
+export const AudioGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  padding: 2em;;
+  padding-top: 0.5em;
+`;
+
+export const AudioList = styled.div`
+  display: block;
+  padding: 20px;
+`;
+
+export const AudioItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const AudioThumbnail = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  margin-bottom: 10px;
+  border-radius: 6px;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+`;
