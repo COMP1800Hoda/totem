cdcb444ce3eaf3d1ce4e1defff68db71d9ccaa29
Victor Yang <vyang11@my.bcit.ca>
2025-02-21 11:28:02 -0800

    updated routes

--- totem-client-front/src/App.tsx
+++ totem-client-front/src/App.tsx
@@ -5,8 +5,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from './pages/home/HomePage.tsx';
 import NotFoundPage from './pages/notFound/NotFoundPage.tsx';
 import { ReadPage } from './pages/read/ReadPage.tsx';
-import Catalogue from './pages/catalogue/CataloguePage.tsx';
+import BooksPage from './pages/book/BooksPage.tsx';
+import MyBooksPage from './pages/myBooks/MyBooksPage.tsx';
 import ProfilePage from './pages/profile/ProfilePage.tsx';
+import AudioPage from './pages/audio/AudioPage.tsx';
+import MyAudioPage from './pages/myAudio/MyAudioPage.tsx';
+import SearchPage from './pages/search/SearchPage.tsx';
 
 const App: React.FC = () => (
   <Router>
@@ -14,8 +18,12 @@ const App: React.FC = () => (
       <Route path="/" element={<HomePage />} />
       <Route path="/read" element={<ReadPage />} />
       <Route path="*" element={<NotFoundPage />} />
-      <Route path="/c" element={<Catalogue />} />
-      <Route path="/p" element={<ProfilePage />} />
+      <Route path="/my-books" element={<MyBooksPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/audio" element={<AudioPage />} />
+      <Route path="/my-audio" element={<MyAudioPage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/books" element={<BooksPage />} />
     </Routes>
 
   </Router>
