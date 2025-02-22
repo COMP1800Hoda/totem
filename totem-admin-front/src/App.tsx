45fdf2ee00d0a366a4e504b879db42f4710abae9
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-02-21 16:17:11 -0800

    render images in db to dummy frontend page

--- totem-admin-front/src/App.tsx
+++ totem-admin-front/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import FileUpload from './fileUpload.tsx'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import FileUpload from './fileUpload.tsx'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   return (
+//     <FileUpload />
+//   );
+// }
+
+// export default App
+
+import React from "react";
+import StorybookGrid from "./StorybookGrid";
 
 function App() {
   return (
-    <FileUpload />
+    <div>
+      <StorybookGrid />
+    </div>
   );
 }
 
-export default App
+export default App;
