a18839d57b8ee9653fa473e90a86106907167eb9
Caitlynchen <144745317+Caitlynchen@users.noreply.github.com>
2025-03-05 16:34:55 -0800

    add homepage

--- totem-admin-front/src/App.tsx
+++ totem-admin-front/src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import FileUpload from './fileUpload.tsx'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AdminHomePage from './Adminhome.tsx';
+import FileUpload from './fileUpload.tsx';
+import './App.css';
 
 function App() {
   return (
-    <FileUpload />
+    <Router>
+      <div className="App">
+        {/* Navigation Links */}
+        {/* <nav>
+          <Link to="/" className="nav-link">Admin Home</Link>
+          <Link to="/file-upload" className="nav-link">File Upload</Link>
+        </nav> */}
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<AdminHomePage />} />
+          <Route path="/file-upload" element={<FileUpload />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
