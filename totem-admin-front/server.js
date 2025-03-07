e8aee92621383df0ac15c2dd89793b987dfd5e59
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-03-07 14:03:08 -0800

    Manually merged changes from Admin/Admin_login_page_Phuong

---
+++ totem-admin-front/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import nodeMailer from 'nodemailer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+//Nodemailer
+
+const transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    },
+});
+
+app.post('/reset-password', async(req, res) => {
+    const {email} = req.body;
+    // Check if email is valid
+    if(!email){
+        return res.status(400).json({message: "Email is required"});
+    }
+
+    const resetLink = `http://localhost:5173/editPassword?email=${encodeURIComponent(email)}`;
+    //Set up email data
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: "Reset Password Request for Totem Children Storybook",
+        text: `Click on the link to reset your password ${resetLink}. This link will redirect you to the reset password page.`,
+    };
+
+    try{
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({message: "Email sent successfully"});
+        console.log("Email sent successfully");
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Error sending email"});
+    }
+})
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
--- totem-admin-front/src/database.js
+++ totem-admin-front/src/database.js
@@ -1,13 +1,12 @@
-// Import Parse SDK -> change this
-/**
- * Vite automatically externalizes the events module for browser compatibility. 
- * The Parse SDK relies on events.EventEmitter, which isn't available in Vite's browser environment.
- * explicitly use the browser version of Parse:
- */
 import Parse from "parse/dist/parse.min.js";
 
+
 // Initialize with your Back4app keys
-Parse.initialize("XWNVzANvs7w6pYMl4fZWLCcikgXdMvCZhEnI48sH", "NFHym7RCYmwrHL2Ohqr7769mcUt66ikBF13liUdt");  
-Parse.serverURL = 'https://parseapi.back4app.com';
+Parse.initialize(
+    import.meta.env.VITE_APP_ID,
+    import.meta.env.VITE_JS_KEY
+);
+
+Parse.serverURL = import.meta.env.VITE_SERVER_URL;
 
 export default Parse;
