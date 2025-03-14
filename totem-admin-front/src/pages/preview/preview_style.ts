e1bc5fe2dee93d6cf65ad2492c1d144d957d5cef
Caitlynchen <144745317+Caitlynchen@users.noreply.github.com>
2025-03-14 13:38:29 -0700

    Preview Page done

--- totem-admin-front/src/pages/preview/preview_style.ts
+++ totem-admin-front/src/pages/preview/preview_style.ts
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 
 export const BookContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+  // display: flex;
+  // flex-direction: column;
+  width: 570px;
+  justify-content: center;
   min-height: 100vh; /* Ensure the container takes the full height of the viewport */
   padding: 1em;
   margin: 0;
   background-color: #F8F0E9; /* Light background color for the page */
 
   /* If you want the content to stretch and fill available space */
-  main {
-    flex: 1;
-  }
+  // main {
+  //   flex: 1;
+   
+  // }
 `;
 
 export const BackButton = styled.button`
@@ -22,6 +25,7 @@ export const BackButton = styled.button`
   background: none;
   border: none;
   cursor: pointer;
+  width: 100%;
 `;
 
 export const BookCard = styled.div`
@@ -30,11 +34,13 @@ export const BookCard = styled.div`
   padding: 16px;
   background: white;
   border-radius: 8px;
+  width:auto;
+  
 `;
 
 export const BookCover = styled.img`
   width: 100px;
-  height: auto;
+  height: 150px;
   border-radius: 8px;
 `;
 
@@ -66,10 +72,10 @@ export const Tag = styled.span`
   font-size: 12px;
 `;
 
-export const ReadButton = styled.button`
+export const UploadButton = styled.button`
   width: 100%;
   margin-top: 16px;
-  background: #8b5a2b;
+  background: #DECBB7;
   color: white;
   padding: 10px;
   border: none;
@@ -86,3 +92,43 @@ export const BookInfo = styled.div`
   font-size: 14px;
   color: #666;
 `;
+
+
+export const ThumbnailContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr)); 
+  gap: 0; /* Remove gap between items */
+  padding: 0; /* Remove padding */
+  margin: 0; /* Remove margin */
+`;
+
+
+export const ThumbnailWrapper = styled.div`
+  flex: 0 0 auto;
+  width: 100px;
+  border: none
+  padding: 0;
+  text-align: center;
+`;
+
+
+export const BookContentImg = styled.img`
+  width: 100%;
+  height: auto;
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05); // Slightly enlarge on hover
+  }
+  `;
+
+ export const FileNameContainer = styled.p`
+  word-break: break-word; /* Break long words */
+  overflow-wrap: break-word; /* Ensure wrapping */
+  white-space: normal; /* Allow text to wrap */
+  max-width: 150px; /* Adjust based on your layout */
+  margin: 0; /* Remove default margin */
+  font-size: 10px;
+`;
+
