cc0ae07a9a215ba193ff18fe3cbe8d4696a57825
Victor Yang <vyang11@my.bcit.ca>
2025-03-17 11:08:39 -0700

    Modified Header and Footer

--- totem-client-front/src/components/footer/Footer.tsx
+++ totem-client-front/src/components/footer/Footer.tsx
@@ -1,36 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router';  // Correct import from react-router-dom
-import { 
-  FooterContainer, 
-  IconWrapper, 
-  IconStyle, 
-  HomeIcon, 
-  BookOpenIcon, 
-  HeadphonesIcon, 
-  SearchIcon, 
-  UserIcon 
-} from './Footer.styled';
+import React from "react";
+import { Link } from "react-router"; // Corrected import
+import {
+  FooterContainer,
+  IconWrapper,
+  IconStyle,
+  HomeIcon,
+  BookOpenIcon,
+  HeadphonesIcon,
+  SearchIcon,
+  UserIcon,
+} from "./Footer.styled";
 
 const Footer: React.FC = () => {
   return (
     <FooterContainer>
       <IconWrapper>
-        <Link to="/">
+        <Link to="/profile">
           <IconStyle>
-            <HomeIcon />
-            <span>Home</span>
-          </IconStyle>
-        </Link>
-        <Link to="/my-books">
-          <IconStyle>
-            <BookOpenIcon />
-            <span>My Books</span>
-          </IconStyle>
-        </Link>
-        <Link to="/my-audio">
-          <IconStyle>
-            <HeadphonesIcon />
-            <span>My Audio</span>
+            <UserIcon />
+            <span>Profile</span>
           </IconStyle>
         </Link>
         <Link to="/search">
@@ -39,10 +27,22 @@ const Footer: React.FC = () => {
             <span>Search</span>
           </IconStyle>
         </Link>
-        <Link to="/profile">
+        <Link to="/my-audio">
           <IconStyle>
-            <UserIcon />
-            <span>Profile</span>
+            <HeadphonesIcon />
+            <span>My Audio</span>
+          </IconStyle>
+        </Link>
+        <Link to="/my-books">
+          <IconStyle>
+            <BookOpenIcon />
+            <span>My Books</span>
+          </IconStyle>
+        </Link>
+        <Link to="/">
+          <IconStyle>
+            <HomeIcon />
+            <span>Home</span>
           </IconStyle>
         </Link>
       </IconWrapper>
