2e1a20dcb389f4d2af43f3fcfa8450f3be935b5f
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-03-28 11:38:10 -0700

    add headers to pages

--- totem-admin-front/src/pages/add-admin/ManageAdminDashboard.tsx
+++ totem-admin-front/src/pages/add-admin/ManageAdminDashboard.tsx
@@ -4,6 +4,8 @@ import { Table, Button, Modal } from 'react-bootstrap';
 import Parse from '../../database';
 import AddAdminModal from './AddAdminModal';
 import EditAdminModal from './EditAdminModal';
+import { Header } from '../../components/header/Header';
+import { Container } from '../../components/Container.tsx';
 
 interface Admin {
   id: string;
@@ -26,7 +28,7 @@ const AdminProfile = () => {
       const results = await query.find(); // fetch all the admins in db
 
       //map results into the state
-      const adminList = results.map((admin) => ({
+      const adminList = results.map((admin: Parse.Object) => ({
         id: admin.id, // Store Parse Object ID
         name: admin.get('admin_name'),
         role: admin.get('admin_role'),
@@ -62,117 +64,124 @@ const AdminProfile = () => {
 
   return (
     <div className="container mt-4 px-4">
-      {/* Table */}
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: '1rem',
-        }}
-      >
-        <h2 style={{ margin: 0 }}>Admin Dashboard</h2>
-        <Button
-          variant="primary"
-          size="sm"
+      <Header />
+      <Container>
+        {/* Table */}
+        <div
           style={{
-            width: '110px',
-            height: '40px',
-            fontSize: '14px',
             display: 'flex',
+            justifyContent: 'space-between',
             alignItems: 'center',
-            justifyContent: 'center',
+            marginTop: '3rem',
+            marginBottom: '1rem',
           }}
-          onClick={() => setShowAddModal(true)}
         >
-          + Add Admin
-        </Button>
-      </div>
-      <Table bordered hover responsive>
-        <thead className="bg-light">
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>EDIT</th>
-            <th>DELETE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {admins.map((admin) => (
-            <tr key={admin.id}>
-              <td>{admin.name}</td>
-              <td>{admin.email}</td>
-              <td>{admin.role}</td>
-              <td>
-                <Button
-                  variant="success"
-                  size="sm"
-                  onClick={() => {
-                    setSelectedAdmin(admin), setShowEditModal(true);
-                  }}
-                >
-                  EDIT
-                </Button>
-              </td>
-              <td>
-                <Button
-                  variant="danger"
-                  size="sm"
-                  onClick={() => {
-                    setAdminToDelete(admin.id);
-                    setShowConfirmModal(true);
-                  }}
-                >
-                  DELETE
-                </Button>
-              </td>
+          <h2 style={{ margin: 0 }}>Admin Dashboard</h2>
+          <Button
+            variant="primary"
+            size="sm"
+            style={{
+              width: '110px',
+              height: '40px',
+              fontSize: '14px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onClick={() => setShowAddModal(true)}
+          >
+            + Add Admin
+          </Button>
+        </div>
+        <Table bordered hover responsive>
+          <thead className="bg-light">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>EDIT</th>
+              <th>DELETE</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {admins.map((admin) => (
+              <tr key={admin.id}>
+                <td>{admin.name}</td>
+                <td>{admin.email}</td>
+                <td>{admin.role}</td>
+                <td>
+                  <Button
+                    variant="success"
+                    size="sm"
+                    onClick={() => {
+                      setSelectedAdmin(admin), setShowEditModal(true);
+                    }}
+                  >
+                    EDIT
+                  </Button>
+                </td>
+                <td>
+                  <Button
+                    variant="danger"
+                    size="sm"
+                    onClick={() => {
+                      setAdminToDelete(admin.id);
+                      setShowConfirmModal(true);
+                    }}
+                  >
+                    DELETE
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
 
-      <AddAdminModal
-        show={showAddModal}
-        onClose={() => setShowAddModal(false)}
-        onAdminAdded={fetchAdmins}
-      />
-      {selectedAdmin && (
-        <EditAdminModal
-          show={showEditModal}
-          onClose={() => setShowEditModal(false)}
-          admin={selectedAdmin}
-          onAdminUpdated={fetchAdmins}
+        <AddAdminModal
+          show={showAddModal}
+          onClose={() => setShowAddModal(false)}
+          onAdminAdded={fetchAdmins}
         />
-      )}
+        {selectedAdmin && (
+          <EditAdminModal
+            show={showEditModal}
+            onClose={() => setShowEditModal(false)}
+            admin={selectedAdmin}
+            onAdminUpdated={fetchAdmins}
+          />
+        )}
 
-      {/* Confirm Modal */}
-      <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirm Deletion</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Are you sure you want to delete Admin{' '}
-          <strong>
-            {admins.find((admin) => admin.id === adminToDelete)?.name ||
-              'this admin'}
-          </strong>
-          ?
-        </Modal.Body>
-        <Modal.Footer>
-          <div className="d-flex gap-5 justify-content-center w-100">
-            <Button
-              variant="secondary"
-              onClick={() => setShowConfirmModal(false)}
-            >
-              Cancel
-            </Button>
-            <Button variant="danger" onClick={handleDelete}>
-              Delete
-            </Button>
-          </div>
-        </Modal.Footer>
-      </Modal>
+        {/* Confirm Modal */}
+        <Modal
+          show={showConfirmModal}
+          onHide={() => setShowConfirmModal(false)}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Deletion</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to delete Admin{' '}
+            <strong>
+              {admins.find((admin) => admin.id === adminToDelete)?.name ||
+                'this admin'}
+            </strong>
+            ?
+          </Modal.Body>
+          <Modal.Footer>
+            <div className="d-flex gap-5 justify-content-center w-100">
+              <Button
+                variant="secondary"
+                onClick={() => setShowConfirmModal(false)}
+              >
+                Cancel
+              </Button>
+              <Button variant="danger" onClick={handleDelete}>
+                Delete
+              </Button>
+            </div>
+          </Modal.Footer>
+        </Modal>
+      </Container>
     </div>
   );
 };
--- totem-admin-front/src/pages/file-upload/fileUpload.tsx
+++ totem-admin-front/src/pages/file-upload/fileUpload.tsx
@@ -3,7 +3,8 @@ import ImageKit from 'imagekit';
 import Parse from '../../database';
 import './fileUpload.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Header } from '../../components/header/Header';
+import { Container } from '../../components/Container.tsx';
 interface FileData {
   file: File;
   name: string;
@@ -301,259 +302,264 @@ const FileUpload: React.FC = () => {
 
   return (
     <div className="App container mt-4 px-4 ">
-      <h2 className="upload_header">Upload New Book</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Book Title</label>
-          <input
-            type="text"
-            value={bookTitle}
-            onChange={(e) => setBookTitle(e.target.value)}
-            //required
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Book ID</label>
-          <input
-            type="text"
-            value={bookId}
-            onChange={(e) => setBookId(e.target.value)}
-            //required
-          />
-        </div>
-        <div className="form-group">
-          <label>Age</label>
-          <select
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-            className="form-control w-auto px-4"
-          >
-            <option value="0~2">0~2</option>
-            <option value="3~4">3~4</option>
-            <option value="5~6">5~6</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label>Genre</label>
-          {genres.map((genre, index) => (
-            <div
-              key={index}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                marginBottom: '5px',
-              }}
-            >
-              <input
-                type="text"
-                value={genre}
-                onChange={(e) => handleGenreInputChange(index, e.target.value)}
-                placeholder="Enter genre..."
-                style={{ marginRight: '10px' }}
-              />
-              <div className="input-with-icon">
-                <button
-                  type="button"
-                  className="delete-button"
-                  onClick={() => handleRemoveGenre(index)}
-                  aria-label="Remove genre"
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </button>
-              </div>
-            </div>
-          ))}
-
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <button
-              type="button"
-              onClick={handleAddGenre}
-              className="Addbutton w-auto px-3"
-            >
-              Add
-            </button>
+      <Header />
+      <Container>
+        <h2 className="upload_header mt-5">Upload New Book</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Book Title</label>
+            <input
+              type="text"
+              value={bookTitle}
+              onChange={(e) => setBookTitle(e.target.value)}
+              //required
+            />
           </div>
-        </div>
-        <div className="form-group">
-          <label>Created by</label>
-          {creators.map((creator, index) => (
-            <div
-              key={index}
-              className="form-row"
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                marginBottom: '5px',
-              }}
+
+          <div className="form-group">
+            <label>Book ID</label>
+            <input
+              type="text"
+              value={bookId}
+              onChange={(e) => setBookId(e.target.value)}
+              //required
+            />
+          </div>
+          <div className="form-group">
+            <label>Age</label>
+            <select
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              className="form-control w-auto px-4"
             >
-              {creator.role === 'Other' ? (
-                <>
-                  <input
-                    type="text"
-                    placeholder="Custom role name"
-                    value={creator.customRole || ''}
-                    onChange={(e) =>
-                      handleCreatorChange(index, 'customRole', e.target.value)
-                    }
-                    style={{ marginRight: '5px' }}
-                  />
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    value={creator.name}
-                    onChange={(e) =>
-                      handleCreatorChange(index, 'name', e.target.value)
-                    }
-                  />
-                </>
-              ) : (
-                <>
-                  <select
-                    value={creator.role}
-                    onChange={(e) =>
-                      handleCreatorChange(index, 'role', e.target.value)
-                    }
-                    style={{ marginRight: '5px' }}
+              <option value="0~2">0~2</option>
+              <option value="3~4">3~4</option>
+              <option value="5~6">5~6</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label>Genre</label>
+            {genres.map((genre, index) => (
+              <div
+                key={index}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginBottom: '5px',
+                }}
+              >
+                <input
+                  type="text"
+                  value={genre}
+                  onChange={(e) =>
+                    handleGenreInputChange(index, e.target.value)
+                  }
+                  placeholder="Enter genre..."
+                  style={{ marginRight: '10px' }}
+                />
+                <div className="input-with-icon">
+                  <button
+                    type="button"
+                    className="delete-button"
+                    onClick={() => handleRemoveGenre(index)}
+                    aria-label="Remove genre"
                   >
-                    <option value="">Select role</option>
-                    <option value="Author">Author</option>
-                    <option value="Poet">Poet</option>
-                    <option value="Illustrator">Illustrator</option>
-                    <option value="Book Cover Designer">
-                      Book Cover Designer
-                    </option>
-                    <option value="Other">Other</option>
-                  </select>
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    value={creator.name}
-                    onChange={(e) =>
-                      handleCreatorChange(index, 'name', e.target.value)
-                    }
-                    style={{ marginRight: '3px' }}
-                  />
-                </>
-              )}
-              <div className="input-with-icon">
-                <button
-                  className="delete-button"
-                  type="button"
-                  onClick={() => handleRemoveCreator(index)}
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </button>
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </div>
               </div>
+            ))}
+
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <button
+                type="button"
+                onClick={handleAddGenre}
+                className="Addbutton w-auto px-3"
+              >
+                Add
+              </button>
             </div>
-          ))}
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <button
-              type="button"
-              onClick={handleAddCreator}
-              className="Addbutton w-auto px-3"
-            >
-              Add
-            </button>
           </div>
-        </div>
-        <div className="form-group">
-          <label>Publisher</label>
-          <input
-            type="text"
-            placeholder="Publisher"
-            value={publisher}
-            onChange={(e) => setPublisher(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label>Published</label>
-          <input
-            type="text"
-            value={published}
-            onChange={(e) => setPublished(e.target.value)}
-            placeholder="Published"
-          />
-        </div>
+          <div className="form-group">
+            <label>Created by</label>
+            {creators.map((creator, index) => (
+              <div
+                key={index}
+                className="form-row"
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginBottom: '5px',
+                }}
+              >
+                {creator.role === 'Other' ? (
+                  <>
+                    <input
+                      type="text"
+                      placeholder="Custom role name"
+                      value={creator.customRole || ''}
+                      onChange={(e) =>
+                        handleCreatorChange(index, 'customRole', e.target.value)
+                      }
+                      style={{ marginRight: '5px' }}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      value={creator.name}
+                      onChange={(e) =>
+                        handleCreatorChange(index, 'name', e.target.value)
+                      }
+                    />
+                  </>
+                ) : (
+                  <>
+                    <select
+                      value={creator.role}
+                      onChange={(e) =>
+                        handleCreatorChange(index, 'role', e.target.value)
+                      }
+                      style={{ marginRight: '5px' }}
+                    >
+                      <option value="">Select role</option>
+                      <option value="Author">Author</option>
+                      <option value="Poet">Poet</option>
+                      <option value="Illustrator">Illustrator</option>
+                      <option value="Book Cover Designer">
+                        Book Cover Designer
+                      </option>
+                      <option value="Other">Other</option>
+                    </select>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      value={creator.name}
+                      onChange={(e) =>
+                        handleCreatorChange(index, 'name', e.target.value)
+                      }
+                      style={{ marginRight: '3px' }}
+                    />
+                  </>
+                )}
+                <div className="input-with-icon">
+                  <button
+                    className="delete-button"
+                    type="button"
+                    onClick={() => handleRemoveCreator(index)}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </div>
+              </div>
+            ))}
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <button
+                type="button"
+                onClick={handleAddCreator}
+                className="Addbutton w-auto px-3"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div className="form-group">
+            <label>Publisher</label>
+            <input
+              type="text"
+              placeholder="Publisher"
+              value={publisher}
+              onChange={(e) => setPublisher(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Published</label>
+            <input
+              type="text"
+              value={published}
+              onChange={(e) => setPublished(e.target.value)}
+              placeholder="Published"
+            />
+          </div>
+
+          <div className="form-group">
+            <label>ISBN</label>
+            <input
+              type="text"
+              value={isbn}
+              placeholder="ISBN"
+              onChange={(e) => setISBN(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Abstract</label>
+            <textarea
+              value={abstract}
+              placeholder="Abstract"
+              onChange={(e) => setAbstract(e.target.value)}
+            />
+          </div>
+        </form>
 
         <div className="form-group">
-          <label>ISBN</label>
+          <label>Book Cover</label>
           <input
-            type="text"
-            value={isbn}
-            placeholder="ISBN"
-            onChange={(e) => setISBN(e.target.value)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Abstract</label>
-          <textarea
-            value={abstract}
-            placeholder="Abstract"
-            onChange={(e) => setAbstract(e.target.value)}
-          />
-        </div>
-      </form>
-
-      <div className="form-group">
-        <label>Book Cover</label>
-        <input
-          type="file"
-          ref={coverInputRef}
-          onChange={handleCoverChange}
-          accept="image/*"
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Book Content images</label>
-        <div className="file-input-container">
-          <i className="fas fa-cloud-upload-alt icon"></i>
-          <label htmlFor="file-upload" className="custom-file-choose">
-            Select Folder
-          </label>
-
-          <input
-            ref={contentInputRef}
-            id="file-upload"
             type="file"
-            onChange={handleFileChange}
-            style={{ display: 'none' }}
+            ref={coverInputRef}
+            onChange={handleCoverChange}
             accept="image/*"
-            multiple
             required
           />
         </div>
-      </div>
-      <div className="upload-container">
-        {folderName && <p>Selected Folder: {folderName}</p>}
-        <div>
-          {files.map((file, index) => (
-            <div key={index} className="preview-container">
-              <span>{file.name}</span>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <span style={{ fontSize: '16px', marginRight: '10px' }}>
-                  {file.size}{' '}
-                </span>
-                <button
-                  onClick={() => handleRemove(index)}
-                  className="preview-remove-Button"
-                >
-                  <i className="fa-solid fa-xmark"></i>
-                </button>
-              </div>
-            </div>
-          ))}
+        <div className="form-group">
+          <label>Book Content images</label>
+          <div className="file-input-container">
+            <i className="fas fa-cloud-upload-alt icon"></i>
+            <label htmlFor="file-upload" className="custom-file-choose">
+              Select Folder
+            </label>
+
+            <input
+              ref={contentInputRef}
+              id="file-upload"
+              type="file"
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+              accept="image/*"
+              multiple
+              required
+            />
+          </div>
         </div>
-      </div>
-      <div className="button-row ">
-        <button type="submit">Preview</button>
-        <button type="submit" onClick={handleSubmit}>
-          Upload
-        </button>
-      </div>
+        <div className="upload-container">
+          {folderName && <p>Selected Folder: {folderName}</p>}
+          <div>
+            {files.map((file, index) => (
+              <div key={index} className="preview-container">
+                <span>{file.name}</span>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <span style={{ fontSize: '16px', marginRight: '10px' }}>
+                    {file.size}{' '}
+                  </span>
+                  <button
+                    onClick={() => handleRemove(index)}
+                    className="preview-remove-Button"
+                  >
+                    <i className="fa-solid fa-xmark"></i>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="button-row ">
+          <button type="submit">Preview</button>
+          <button type="submit" onClick={handleSubmit}>
+            Upload
+          </button>
+        </div>
+      </Container>
     </div>
   );
 };
