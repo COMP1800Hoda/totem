55d8f831b6ad3079c77f5b23ee26c46a8cab2474
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-04-01 17:50:56 -0700

    change dockerfile

--- totem-admin-front/vite.config.ts
+++ totem-admin-front/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'node:crypto': require.resolve('crypto-browserify'),
+      'node:assert': require.resolve('browser-assert'),
+      'node:url': require.resolve('url'),
+      'node:path': require.resolve('path-browserify'),
+    },
+  },
+});
