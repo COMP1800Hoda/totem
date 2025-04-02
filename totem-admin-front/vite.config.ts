2b6590fbd7f6fcb2135a843790f2e4f4b8b8e2be
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-04-01 18:00:12 -0700

    change vite.config.ts for ES module

--- totem-admin-front/vite.config.ts
+++ totem-admin-front/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'node:crypto': require.resolve('crypto-browserify'),
-      'node:assert': require.resolve('browser-assert'),
-      'node:url': require.resolve('url'),
-      'node:path': require.resolve('path-browserify'),
+      'node:crypto': fileURLToPath(new URL('./node_modules/crypto-browserify', import.meta.url)),
+      'node:assert': fileURLToPath(new URL('./node_modules/browser-assert', import.meta.url)),
+      'node:url': fileURLToPath(new URL('./node_modules/url', import.meta.url)),
+      'node:path': fileURLToPath(new URL('./node_modules/path-browserify', import.meta.url)),
     },
   },
 });
