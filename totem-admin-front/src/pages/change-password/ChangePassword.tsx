98fbe998ce887756372ed6a95fbd7094914e2c22
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-04-04 11:40:02 -0700

    remove useLocation in ChangePassword.tsx

--- totem-admin-front/src/pages/change-password/ChangePassword.tsx
+++ totem-admin-front/src/pages/change-password/ChangePassword.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import Parse from '../../database';
