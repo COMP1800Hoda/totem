1817578e2f50c3c133eeddbc178b7ca00cc148b8
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-04-06 20:31:53 -0700

    test deployment with client page again

--- totem-client-front/src/pages/search/SearchPage.tsx
+++ totem-client-front/src/pages/search/SearchPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Header } from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSearch } from './SearchPageLogic.ts';
 import { COLORS } from '../../constants/colors.ts';
 import {
